b988db31643fbf520515baecb350ea6506f17bf6
[nobita] <[936446507@qq.com]>
2017-10-22 15:38:51 +0800

    vue-music-app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        <m-header></m-header>
+        
+    </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+export default {
+    name: 'app',
+    components: {
+        MHeader
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
